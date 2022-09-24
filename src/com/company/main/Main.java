bf7e487762e466476c4e8fdd0db2f2f9c70e5b22
Vladimirrom <vladimirrom0707@gmail.com>
2022-09-24 22:15:17 +0300

    Initial commit

---
+++ src/com/company/main/Main.java
@@ -0,0 +1,12 @@
+package com.company.main;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> strings = new LinkedList<>();
+        strings = null;
+        strings.add(null);
+    }
+}
